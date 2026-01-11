37caf3b61d9425698a2ba3e756cdcad2c4486d34
CromJo <tjk851225@naver.com>
2026-01-11 19:40:00 +0900

    Add 1. 기본 프로젝트 추가.

---
+++ Source/Panic_Room/Panic_Room.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Panic_Room.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Panic_Room, "Panic_Room" );
+ 
---
+++ Source/Panic_Room/Panic_RoomGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Panic_RoomGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APanic_RoomGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APanic_RoomGameMode();
+};
+
+
+
