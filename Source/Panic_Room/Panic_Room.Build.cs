d593c032dacbb7f98967ebbab347686e10d97dee
Keon Kang <kitchengun124@gmail.com>
2026-01-18 21:26:29 +0900

    cpp h 파일 폴더 정리
    actor component
    Character
    framework
    폴더 만들어서 정리

--- Source/Panic_Room/Panic_Room.Build.cs
+++ Source/Panic_Room/Panic_Room.Build.cs
@@ -15,6 +15,15 @@ public class Panic_Room : ModuleRules
             "UMG", "Slate", "SlateCore"
         });
 
+        // Source 폴더 내의 모든 하위 폴더를 인클루드 경로로 잡게 함
+        PublicIncludePaths.AddRange(
+        new string[] {
+            "Panic_Room",           // 기본 모듈 경로
+            "Panic_Room/Character", // Character 폴더를 기본 include 경로로 추가
+            "Panic_Room/ActorComponent",
+            "Panic_Room/Framework"
+        }
+    );
         PrivateDependencyModuleNames.AddRange(new string[] { });
     }
 }
