f1ae260569930ab98754df549ccec720a1beb6cd
Keon Kang <kitchengun124@gmail.com>
2026-01-28 20:15:41 +0900

    PlayerState 충돌 수정
    충돌난 파일 합침

--- Source/Panic_Room/Character/BasicPlayerState.cpp
+++ Source/Panic_Room/Character/BasicPlayerState.cpp
@@ -4,21 +4,75 @@
 #include "Character/BasicPlayerState.h"
 #include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
+#include "Net/UnrealNetwork.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineIdentityInterface.h"
 
 ABasicPlayerState::ABasicPlayerState()
 {
-    // ASC 생성
-    ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 
-    // 멀티플레이어 최적화: PlayerState에 있을 경우 소유 클라이언트에만 상세 이펙트 전달, 타인에겐 최소화
-    ASC->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	// ASC 생성
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 
-    // AttributeSet 생성
-    AttributeSet = CreateDefaultSubobject<UAttributeSet>(TEXT("AttributeSet"));
+	// 멀티플레이어 최적화: PlayerState에 있을 경우 소유 클라이언트에만 상세 이펙트 전달, 타인에겐 최소화
+	ASC->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
-    // 네트워크 갱신 빈도 설정 (멀티플레이어 최적화)
-    NetUpdateFrequency = 100.0f;
+	// AttributeSet 생성
+	AttributeSet = CreateDefaultSubobject<UAttributeSet>(TEXT("AttributeSet"));
+
+	// 복제 활성화
+	bReplicates = true;
+
+	// 네트워크 갱신 빈도 설정 (멀티플레이어 최적화)
+	NetUpdateFrequency = 100.0f;
 }
+
+void ABasicPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ABasicPlayerState, SteamPlayerName);
+}
+
+void ABasicPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 세션
+	if (HasAuthority())
+	{
+		FetchSteamPlayerName();
+	}
+}
+
+void ABasicPlayerState::FetchSteamPlayerName()
+{
+	IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
+	if (OnlineSubsystem)
+	{
+		IOnlineIdentityPtr IdentityInterface = OnlineSubsystem->GetIdentityInterface();
+		if (IdentityInterface.IsValid())
+		{
+			FString PlayerName = IdentityInterface->GetPlayerNickname(0);
+			if (!PlayerName.IsEmpty())
+			{
+				SteamPlayerName = PlayerName;
+				OnRep_SteamPlayerName();
+
+				return;
+			}
+		}
+	}
+
+	SteamPlayerName = GetPlayerName();
+	OnRep_SteamPlayerName();
+}
+
+// 세션 검색 완료 시 성공 여부 알림 함수
+void ABasicPlayerState::OnRep_SteamPlayerName()
+{
+	OnSteamNameChanged.Broadcast(SteamPlayerName);
+}
+
 
 UAbilitySystemComponent* ABasicPlayerState::GetAbilitySystemComponent() const
 {
--- Source/Panic_Room/Character/BasicPlayerState.h
+++ Source/Panic_Room/Character/BasicPlayerState.h
@@ -7,9 +7,10 @@
 #include "AbilitySystemInterface.h"
 #include "BasicPlayerState.generated.h"
 
-/**
- * 
- */
+
+ // Steam 이름 변경 시 UI 업데이트 델리게이트 이벤트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSteamNameChanged, const FString&, NewName);
+
 UCLASS()
 class PANIC_ROOM_API ABasicPlayerState : public APlayerState, public IAbilitySystemInterface
 {
@@ -19,10 +20,32 @@ public:
 
 	ABasicPlayerState();
 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 	UFUNCTION(BlueprintCallable, Category = "GAS")
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 	class UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	void FetchSteamPlayerName();
+
+	UPROPERTY(ReplicatedUsing = OnRep_SteamPlayerName, BlueprintReadOnly, Category = "Player")
+	FString SteamPlayerName;
+
+	UFUNCTION()
+	void OnRep_SteamPlayerName();
+
+public:
+	// 유저 이름 변경 업데이트용 이벤트
+	UFUNCTION(BlueprintCallable, Category = "Player")
+	FString GetSteamPlayerName() const { return SteamPlayerName; }
+
+	// 현재 유저 이름 저장변수
+	UPROPERTY(BlueprintAssignable, Category = "Player")
+	FOnSteamNameChanged OnSteamNameChanged;
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GAS")
 	TObjectPtr<class UAbilitySystemComponent> ASC;
