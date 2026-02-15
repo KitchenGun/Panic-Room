ff7fa5bfb1be32cf95aac45d2c54fa80f744af48
Keon Kang <kitchengun124@gmail.com>
2026-02-15 19:31:05 +0900

    Basic ability 구현 basic attribute 구현
    기존 레거시 어빌리티, 어트리튜트 제거
    어빌리티의 경우 공통적으로 사용해야 할 경우를 생각해서 부모를 상속 받아 구현하도록 구현

---
+++ Source/Panic_Room/Ability/GA_Basic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Basic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANIC_ROOM_API UGA_Basic : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
