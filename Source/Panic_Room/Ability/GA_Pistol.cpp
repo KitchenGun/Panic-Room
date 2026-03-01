e70e3b197745d3dff67dc5c09f79a4e11e94f48b
Keon Kang <kitchengun124@gmail.com>
2026-03-01 17:46:43 +0900

    ga_pistol 오류 수정
    해더 위치로 빌드 안되는 문제 수정

--- Source/Panic_Room/Ability/GA_Pistol.cpp
+++ Source/Panic_Room/Ability/GA_Pistol.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Ability/GA_Pistol.h"
 #include "Character/Panic_RoomCharacter.h"
 #include "Character/BasicPlayerState.h"
 #include "ActorComponent/Panic_RoomWeaponComponent.h"
-#include "Ability/GA_Pistol.h"
 
 APanic_RoomCharacter* UGA_Pistol::GetPanicRoomCharacterFromActorInfo() const
 {
